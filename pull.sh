678e4f4e9e6b7b52e58db0551f23fb69d6448785
Jaakkko <jaakkole2@gmail.com>
2020-02-29 20:48:34 +0200

    Added hidden files

--- pull.sh
+++ pull.sh
@@ -1,5 +1,10 @@
 #!/bin/bash
 cp ~/.vimrc vimrc
+cp ~/.bashrc .
+cp ~/.zshrc .
+cp ~/.Xresources .
 cp ~/.config/alacritty/alacritty.yml .
 cp ~/.config/i3/config .
 cp /etc/i3blocks.conf .
+
+cp -r ~/.scripts .
