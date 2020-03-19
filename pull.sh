3c964f2cfe3a01326af1cf1798995b0e026db523
Jaakkko <jaakkole2@gmail.com>
2020-03-19 18:41:36 +0200

    Rofi config. Border color.

--- pull.sh
+++ pull.sh
@@ -6,6 +6,7 @@ cp ~/.imwheelrc .
 cp ~/.Xresources .
 cp ~/.config/alacritty/alacritty.yml .
 cp ~/.config/i3/config .
+cp ~/.config/rofi/config.rasi .
 cp /etc/i3blocks.conf .
 
 cp -r ~/.scripts .
