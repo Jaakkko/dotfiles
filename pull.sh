8ad62820b3eadc74a1580b2263c3281b6a687c70
Jaakkko <jaakkole2@gmail.com>
2020-03-19 20:28:11 +0200

    GTK

--- pull.sh
+++ pull.sh
@@ -8,4 +8,8 @@ cp ~/.config/i3/config .config/i3
 cp ~/.config/i3/i3blocks.conf .config/i3
 cp ~/.config/rofi/config.rasi .config/rofi
 
+# GTK
+cp -r ~/.config/gtk-3.0 .config
+cp ~/.gtkrc-2.0 .
+
 cp -r ~/.scripts .
