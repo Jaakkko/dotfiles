4fe313bbe2784a6c5eff02532c441064f821efda
Jaakkko <jaakkole2@gmail.com>
2020-03-19 20:23:38 +0200

    Update pull.sh. Update config file location

--- pull.sh
+++ pull.sh
@@ -3,9 +3,9 @@ cp ~/.vimrc vimrc
 cp ~/.zshrc .
 cp ~/.imwheelrc .
 cp ~/.Xresources .
-cp ~/.config/alacritty/alacritty.yml .
-cp ~/.config/i3/config .
-cp ~/.config/i3/i3blocks.conf .
-cp ~/.config/rofi/config.rasi .
+cp ~/.config/alacritty/alacritty.yml .config/alacritty
+cp ~/.config/i3/config .config/i3
+cp ~/.config/i3/i3blocks.conf .config/i3
+cp ~/.config/rofi/config.rasi .config/rofi
 
 cp -r ~/.scripts .
