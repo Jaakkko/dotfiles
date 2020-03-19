0f2a74e6afa440ca9b849fb5baefcb690ef8e09a
Jaakkko <jaakkole2@gmail.com>
2020-03-19 21:20:08 +0200

    Cyclic

--- chromeTabs/background.js
+++ chromeTabs/background.js
@@ -1,19 +1,41 @@
 'use strict';
 
 const moveCurrentTab = (direction) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
-        const { id: currentId, index: currentIndex } = tab[0];
-        chrome.tabs.move(currentId, { index: currentIndex + 1 * direction })
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
+        const tabCount = tabs.length
+        chrome.tabs.query({ active: true, currentWindow: true }, (tab) => {
+            const { id: currentId, index: currentIndex } = tab[0];
+            let nextIndex = currentIndex + 1 * direction
+            if (nextIndex < 0) {
+                nextIndex = tabCount - 1
+            }
+            else if (nextIndex === tabCount) {
+                nextIndex = 0
+            }
+
+            chrome.tabs.move(currentId, { index: nextIndex })
+        })
     })
 }
 
 const focusTab = (direction) => {
-    chrome.tabs.query({ active: true, currentWindow: true }, (currentTab) => {
-        const { index: currentIndex } = currentTab[0];
-        chrome.tabs.query({ index: currentIndex + 1 * direction, currentWindow: true }, (focusTab) => {
-            const { id } = focusTab[0];
-            chrome.tabs.update(id, {
-                active: true
+    chrome.tabs.query({ currentWindow: true }, (tabs) => {
+        const tabCount = tabs.length
+        chrome.tabs.query({ active: true, currentWindow: true }, (currentTab) => {
+            const { index: currentIndex } = currentTab[0];
+            let nextIndex = currentIndex + 1 * direction
+            if (nextIndex < 0) {
+                nextIndex = tabCount - 1
+            }
+            else if (nextIndex === tabCount) {
+                nextIndex = 0
+            }
+
+            chrome.tabs.query({ index: nextIndex, currentWindow: true }, (focusTab) => {
+                const { id } = focusTab[0];
+                chrome.tabs.update(id, {
+                    active: true
+                })
             })
         })
     })
