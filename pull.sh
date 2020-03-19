04b7aeaaaef493b83cbd402cf40b23c4e5ba2a96
Jaakkko <jaakkole2@gmail.com>
2020-03-19 18:50:06 +0200

    Move all files into right places.

--- pull.sh
+++ pull.sh
@@ -1,12 +1,11 @@
 #!/bin/bash
 cp ~/.vimrc vimrc
-cp ~/.bashrc .
 cp ~/.zshrc .
 cp ~/.imwheelrc .
 cp ~/.Xresources .
 cp ~/.config/alacritty/alacritty.yml .
 cp ~/.config/i3/config .
+cp ~/.config/i3/i3blocks.conf .
 cp ~/.config/rofi/config.rasi .
-cp /etc/i3blocks.conf .
 
 cp -r ~/.scripts .
