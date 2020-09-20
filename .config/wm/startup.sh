d5cba48a854d97692dcfcca75cb599f19ee9c8aa
Jaakkko <jaakkole2@gmail.com>
2020-09-20 14:31:28 +0300

    Change backend and remove shadows

--- .config/wm/startup.sh
+++ .config/wm/startup.sh
@@ -8,7 +8,15 @@ setxkbmap -layout fi
 
 # Composite manager
 pgrep picom ||
-picom --backend glx --inactive-opacity=1.0 --shadow-radius=0 --shadow-opacity=0 --use-damage --fade-delta 6 --fade-in-step=0.03 --fade-out-step=0.03 &
+picom \
+	--backend xrender \
+	--inactive-opacity=1.0 \
+	--shadow-radius=0 \
+	--shadow-opacity=0 \
+	--fade-delta 6 \
+	--fade-in-step=0.03 \
+	--fade-out-step=0.03 \
+	--frame-opacity=1.0 &
 
 # Scroll
 pgrep imwheel || imwheel -b 45
