f55cb9d0229d33c57242d06e10f841b39b504df6
Jaakkko <jaakkole2@gmail.com>
2020-09-20 11:47:08 +0300

    Inactive opacity

--- .config/wm/startup.sh
+++ .config/wm/startup.sh
@@ -8,7 +8,7 @@ setxkbmap -layout fi
 
 # Composite manager
 pgrep picom ||
-picom --backend glx --shadow-radius=0 --shadow-opacity=0 --use-damage --fade-delta 6 --fade-in-step=0.03 --fade-out-step=0.03 &
+picom --backend glx --inactive-opacity=1.0 --shadow-radius=0 --shadow-opacity=0 --use-damage --fade-delta 6 --fade-in-step=0.03 --fade-out-step=0.03 &
 
 # Scroll
 pgrep imwheel || imwheel -b 45
